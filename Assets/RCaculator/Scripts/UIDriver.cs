e366120b01f13cd3772b7d58f446f49433185d05
GameHole <741419087@qq.com>
2024-04-05 13:44:13 +0800

    添加驱动

---
+++ Assets/RCaculator/Scripts/UIDriver.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+namespace RCaculator
+{
+    public class UIDriver : MonoBehaviour
+    {
+        public UIRCaculator caculator { get;private set; }
+
+        public void Start()
+        {
+            caculator = new UIRCaculator();
+            caculator.LoadProperty(transform);
+        }
+    }
+}
---
+++ Assets/UnitTest/TestUICaculator.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using RCaculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    class TestUICaculator
+    {
+        [Test]
+        public void test()
+        {
+            var ui = PrefabHelper.New("UIDriver").GetComponent<UIDriver>();
+            ui.Start();
+            var c = ui.caculator;
+            Assert.AreSame(ui.transform, c.transform);
+            ui.Start();
+            Assert.AreNotSame(c, ui.caculator);
+        }
+    }
+}
